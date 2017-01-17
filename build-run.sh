2bd9fc19099567cfcf80ce1f08fe3138d48f20c4
Daniël van de Giessen <git@dvdgiessen.nl>
2017-01-17 01:39:13 +0100

    Changed around the structure a bit to separate base and clients, added checks to build-run script.

--- build-run.sh
+++ build-run.sh
@@ -1,16 +1,84 @@
 #!/bin/bash
-set -e
+if [ -z "$BASH" ] ; then
+    echo "This script must be run with bash" >&2
+    exit 1
+fi
+set -eu
+
+# Check for docker
+if [[ -z "$(which docker)" ]] || ! docker version >/dev/null 2>&1; then
+    echo "Docker should be installed and running" >&2
+    exit 1
+fi
+
+# Usage / help text
+print_usage() {
+    echo "Usage: $0 SYNCTOOL PEERCOUNT"
+    echo ""
+    echo "Runs measurements for the given file sync tool and number of peers."
+    echo ""
+    echo "Options:"
+    echo "    -?             Print this message."
+    echo "    SYNCTOOL       Sync tool to run tests for. Can be one of:$(find [^-.]* -mindepth 1 -maxdepth 1 -type f -name Dockerfile -not -path "base/Dockerfile" -printf ' %h' )"
+    echo "    PEERCOUNT      Number of peers to run in addition to the main test runner."
+    echo "                   A value of 0 will run the tests in stand-alone mode."
+    echo ""
+    echo "Examples:"
+    echo "Run the tests on Dropbox with 1 peer:"
+    echo "    $0 dropbox 1"
+    echo "Run the tests on Resilio Sync with 5 peers:"
+    echo "    $0 resilio-sync 5"
+    echo ""
+    exit 1
+}
+
+# Define argument variables
+SYNCTOOL=""
+PEERCOUNT=""
+
+# Check for arguments
+if [[ $# -lt 2 ]] ; then
+    print_usage
+fi
+I=0
+while [[ "$#" -ge 1 ]] ; do
+    case "$1" in
+        "-?"|-h|--help)
+            print_usage
+        ;;
+        *)
+            case "$I" in
+                0)
+                    SYNCTOOL="$1"
+                ;;
+                1)
+                    printf -v PEERCOUNT '%d' "$1"
+                ;;
+                *)
+                    echo "Unknown parameter \"$1\"" >&2
+                    exit 1
+                ;;
+            esac
+            I=$((I+1))
+        ;;
+    esac
+    shift
+done
+
+# Check the sync tool
+if [[ ! -f "$SYNCTOOL/Dockerfile" ]] ; then
+    echo "Cannot find Dockerfile for \"$SYNCTOOL\"" >&2
+    exit 1
+fi
 
 # Build all images
-echo "[$(date +%s.%N)] Building Docker images ..." >&2
-docker build -t "utw-cn:base" .
-docker build -t "utw-cn:dropbox" dropbox
-
-# Determine number of peers
-printf -v PEERCOUNT '%d' "$1"
+echo "[$(date +%s.%N)] Building base Docker image ..." >&2
+docker build -t "utw-cn:base" base > /dev/null
+echo "[$(date +%s.%N)] Building $SYNCTOOL Docker image ..." >&2
+docker build -t "utw-cn:$SYNCTOOL" "$SYNCTOOL" > /dev/null
 
 # Create the test runner container
-RUNNER=$(docker create -i --tmpfs "/workdir" "utw-cn:dropbox" /test-runner.sh $PEERCOUNT)
+RUNNER=$(docker create -i --tmpfs "/workdir" "utw-cn:$SYNCTOOL" /test-runner.sh "$PEERCOUNT")
 
 # Set up peers
 if [[ $PEERCOUNT -gt 0 ]] ; then
@@ -18,14 +86,14 @@ if [[ $PEERCOUNT -gt 0 ]] ; then
 
     # Create and run peers
     PEERS=()
-    for (( I=1 ; I <= $PEERCOUNT; I++)) ; do
-        PEERS[$(($I - 1))]=$(docker create -ti --tmpfs /workdir --volumes-from "$RUNNER" "utw-cn:dropbox" /test-peer.sh $I)
-        echo -e "  \e[93m-  Press Ctrl-P Ctrl-Q after completing Dropbox setup and pressing ENTER  -\e[0m  " >&2
-        if ! docker start -ai "${PEERS[$(($I - 1))]}" ; then
+    for (( I=1 ; I <= PEERCOUNT; I++)) ; do
+        PEERS[$((I - 1))]=$(docker create -ti --tmpfs /workdir --volumes-from "$RUNNER" "utw-cn:$SYNCTOOL" /test-peer.sh $I)
+        echo -e "  \e[93m-  Press Ctrl-P Ctrl-Q to detach this peer after completing setup  -\e[0m  " >&2
+        if ! docker start -ai "${PEERS[$((I - 1))]}" ; then
             echo -e "[$(date +%s.%N)] \e[91mStart of peer $I failed!\e[0m" >&2
             echo "[$(date +%s.%N)] Removing containers ..." >&2
-            for (( J=1 ; J <= $I; J++)) ; do
-                docker rm -f "${PEERS[$(($J - 1))]}" >/dev/null
+            for (( J=1 ; J <= I; J++)) ; do
+                docker rm -f "${PEERS[$((J - 1))]}" >/dev/null
             done
             docker rm -f "$RUNNER" >/dev/null
             exit 1
@@ -35,15 +103,17 @@ fi
 
 # Start the test runner
 echo "[$(date +%s.%N)] Executing test runner ..." >&2
-if ! docker start -ai "$RUNNER" > results.tar ; then
+if docker start -ai "$RUNNER" > results.tar ; then
+     echo "[$(date +%s.%N)] Saved test results to \"results.tar\"."
+else
     echo -e "[$(date +%s.%N)] \e[91mExecuting test runner failed!\e[0m" >&2
 fi
 
 # Destroy all containers
 echo "[$(date +%s.%N)] Removing containers ..." >&2
 if [[ $PEERCOUNT -gt 0 ]] ; then
-    for (( I=1 ; I <= $PEERCOUNT; I++)) ; do
-        docker rm -f "${PEERS[$(($I - 1))]}" >/dev/null
+    for (( I=1 ; I <= PEERCOUNT; I++)) ; do
+        docker rm -f "${PEERS[$((I - 1))]}" >/dev/null
     done
 fi
 docker rm -f "$RUNNER" >/dev/null
